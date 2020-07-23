5ee5f245c3b2a8676c2461ec237f3377ab8f5f17
ossianmoore <g00351479@gmit.ie>
2020-07-23 11:15:24 +0100

    Created NavBar with routing to relevant generated pages. Routing tested & working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { Item1Component } from './Pages/item1/item1.component';
+import { Item2Component } from './Pages/item2/item2.component';
+import { Item3Component } from './Pages/item3/item3.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'item1', component: Item1Component},
+  {path: 'item2', component: Item2Component},
+  {path: 'item3', component: Item3Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { Item1Component } from './Pages/item1/item1.component';
+import { Item2Component } from './Pages/item2/item2.component';
+import { Item3Component } from './Pages/item3/item3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    Item1Component,
+    Item2Component,
+    Item3Component
   ],
   imports: [
     BrowserModule,
